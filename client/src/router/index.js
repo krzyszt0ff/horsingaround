f6e5195996ba99d997976cec7bb0abd8a9bdda68
zrzepiejewska <166124826+zrzepiejewska@users.noreply.github.com>
2025-12-05 22:39:20 +0100

    Ranking zrobiony bardzo na sztywno, czeka na backend

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -7,6 +7,7 @@ import SignupStep1 from '../views/SignupStep1.vue'
 import SignupStep2 from '../views/SignupStep2.vue'
 import SignupStep3 from '../views/SignupStep3.vue'
 import ProfileView from '../views/ProfileView.vue'
+import RankingView from '../views/RankingView.vue' 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,7 +18,8 @@ const router = createRouter({
     { path: '/signup/step1', name: 'signup-step1', component: SignupStep1 },
     { path: '/signup/step2', name: 'signup-step2', component: SignupStep2 },
     { path: '/signup/step3', name: 'signup-step3', component: SignupStep3 },
-    { path: '/profile', name: 'profile', component: ProfileView },
+    { path: '/profile', name: 'profile', component: ProfileView, meta: {requiresAuth: true} },
+    { path: '/ranking', name: 'ranking', component: RankingView, meta: {requiresAuth: true} }, //Narazie bez wymogu logowania, bo nie działało mi logowanie, więc trzeba zmienić to później
 
     // zostawiamy "home" na przyszłość, np. do ekranu głównego po zalogowaniu
     { path: '/home', name: 'home', component: HomeView },
