2ff4d0a2c8b22de9c3d47ab715abe7ed4bb20434
danpiotr <danowski.piotr1@gmail.com>
2026-01-11 01:01:13 +0100

    + Logika rankingów

--- server/routes/matches.js
+++ server/routes/matches.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import { listMatches, deleteMatch, listMessages, listChats } from '../controllers/matchesController.js';
+import { rankLikes, rankMatches } from '../controllers/rankingController.js';
 
 const router = express.Router();
 
-router.get('/',listMatches);                        //zwraca obiekty w postaci: kod statusu, id_matchu, obiekt z danymi profilu osoby z matcha, treść ostatniej wiadomości last_message
+// PROFIL
+router.get('/',listMatches);                        //zwraca kod statusu, obiekty w postaci: kod statusu, id_matchu, obiekt z danymi profilu osoby z matcha, treść ostatniej wiadomości last_message
 router.delete('/', deleteMatch);                    //zwraca kod statusu
+
+// CHAT
 router.get('/:matchid/messages', listMessages);     //zwraca kod statusu, wiadomości przypisane do czatu danego matcha posortowane malejąco (najpierw najnowsze);
 router.get('/chats', listChats);                    //zwraca kod statusu, matche przypisane do użytkowika, ostatnią wiadomość
+
+// RANKING
+router.get('/rank/likes/:gender', rankLikes);       //zwraca kod statusu, listę użytkowników z największą ilością polubień i ilość polubień
+router.get('/rank/matches/:gender', rankMatches);   //zwraca kod statusu, listę użytkowników z największą ilością matchy i ich ilość
 
 export default router;
